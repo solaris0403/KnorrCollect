2d9588d3597b5fd30201703319a8183619e282af
tony.cao <tonycao@augmentum.com.cn>
2015-10-19 15:20:05 +0800

    Add function that get the Application singleton

--- app/src/main/java/tw/com/knorr/app/KnorrApplication.java
+++ app/src/main/java/tw/com/knorr/app/KnorrApplication.java
@@ -8,9 +8,22 @@ import com.tony.volleydemo.http.utils.VolleyPlus;
  * Created by user on 10/15/15.
  */
 public class KnorrApplication extends Application {
+    private static KnorrApplication instance = new KnorrApplication();
+
     @Override
     public void onCreate() {
         super.onCreate();
+        init();
+    }
+
+    private void init() {
         VolleyPlus.init(this);
+    }
+
+    public static KnorrApplication getInstance() {
+        if (instance == null) {
+            instance = new KnorrApplication();
+        }
+        return instance;
     }
 }
